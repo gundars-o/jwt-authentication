86435a4d1d56d39c0f78038a96df6cb51a0022e2
gundars-o <gundars.ozolins75@gmail.com>
2021-01-07 15:44:17 +0200

    Authenticate Token

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ const posts = [
 ];
 app.get(
     "/posts",
+    authenticateToken,
     ( req, res ) => {
-        res.json( posts );
+        res.json( posts.filter( post => post.username === req.user.name ) );
     }
 );
 app.post(
@@ -29,4 +30,19 @@ app.post(
         res.json( { accessToken: accessToken } );
     }
 );
+function authenticateToken( req, res, next ) {
+    // Bearer TOKEN
+    const authHeader = req.headers[ "authorization" ];
+    const token = authHeader && authHeader.split( " " )[ 1 ];
+    if ( token == null ) return res.sendStatus( 401 );
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        ( err, user ) => {
+            if ( err ) return res.sendStatus( 403 );
+            req.user = user;
+            next();
+        }
+    );
+};
 app.listen( 3000 );
