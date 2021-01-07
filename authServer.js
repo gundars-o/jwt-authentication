c02a2b7fb9a0163a235d9fd97576100b8afc9080
gundars-o <gundars.ozolins75@gmail.com>
2021-01-07 17:54:47 +0200

    Create refresh token.

--- authServer.js
+++ authServer.js
@@ -9,23 +9,12 @@ app.post(
         // Authenticate User
         const username = req.body.username;
         const user = { name: username };
-        const accessToken = jwt.sign( user, process.env.ACCESS_TOKEN_SECRET );
-        res.json( { accessToken: accessToken } );
+        const accessToken = generateAccessToken( user );
+        const refreshToken = jwt.sign( user, process.env.REFRESH_TOKEN_SECRET );
+        res.json( { accessToken: accessToken, refreshToken: refreshToken } );
     }
 );
-function authenticateToken( req, res, next ) {
-    // Bearer TOKEN
-    const authHeader = req.headers[ "authorization" ];
-    const token = authHeader && authHeader.split( " " )[ 1 ];
-    if ( token == null ) return res.sendStatus( 401 );
-    jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN_SECRET,
-        ( err, user ) => {
-            if ( err ) return res.sendStatus( 403 );
-            req.user = user;
-            next();
-        }
-    );
+function generateAccessToken( user ) {
+    return jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "20s" /*10m*/ } );
 };
 app.listen( 4000 );
